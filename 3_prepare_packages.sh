5c4e8b5bf9f6aa8ed0f0587671a9f3bdefd7d3b4
GitHub Action <action@github.com>
2020-12-22 18:22:36 +0800

    r2s

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -3,13 +3,15 @@ rm -rf feeds/packages/libs/libcap/ && svn co https://github.com/openwrt/packages
 rm -rf feeds/packages/lang/golang/ && svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
 sed -i '/enable-jsonc/i\\t--disable-cloud \\' feeds/packages/admin/netdata/Makefile 
 
+mkdir package/openwrt-packages
+
 cd package/lean/
 if [[ `pwd` == *"rk3328"* ]]; then
   rm -rf luci-app-oled/ && git clone https://github.com/NateLol/luci-app-oled
 fi
 [ ! -d luci-app-adguardhome ] && git clone https://github.com/rufengsuixing/luci-app-adguardhome
-[ ! -d luci-app-mentohust ] && git clone https://github.com/BoringCat/luci-app-mentohust
-[ ! -d mentohust ] && svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/mentohust
+#[ ! -d luci-app-mentohust ] && git clone https://github.com/BoringCat/luci-app-mentohust
+#[ ! -d mentohust ] && svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/mentohust
 rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 rm -rf luci-theme-edge/ && git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 rm -rf luci-theme-infinityfreedom/ && git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
---
+++ add_kid.sh
@@ -0,0 +1,4 @@
+cd package/openwrt-packages
+git clone https://github.com/garypang13/luci-app-eqos.git
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+cd ../../
