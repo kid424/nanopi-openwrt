ed054b3e9d7526227e4fae4ecb1b98124a30ce8a
GitHub Action <action@github.com>
2020-12-23 09:18:57 +0800

    folder existance check

--- add_kid.sh
+++ add_kid.sh
@@ -1,4 +1,4 @@
 cd package/openwrt-packages
-git clone https://github.com/garypang13/luci-app-eqos.git
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+[ ! -d luci-app-eqos ] && git clone https://github.com/garypang13/luci-app-eqos.git
+[ ! -d luci-app-jd-dailybonus ] && git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 cd ../../
