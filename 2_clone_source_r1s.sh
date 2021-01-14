5c88277578f762f5cd6c0f2fef2e7af7c420c8d3
kid424 <darkred424@gmail.com>
2021-01-14 17:26:17 +0800

    h5 source code to 19.07.5

--- 2_clone_source_r1s.sh
+++ 2_clone_source_r1s.sh
@@ -3,6 +3,8 @@ sudo cp repo/repo /usr/bin/
 mkdir friendlywrt-$2
 cd friendlywrt-$2
 repo init -u https://github.com/friendlyarm/friendlywrt_manifests -b $1 -m $2.xml --repo-url=https://github.com/friendlyarm/repo --no-clone-bundle --depth=1
+cp ../h5.xml .repo/manifests/h5.xml
+repo init -m h5.xml
 repo sync -c --no-tags --no-clone-bundle -j8
 cd friendlywrt/ && git fetch --unshallow
 git checkout `git branch -va | grep remotes/m | awk '{print $3}' | awk -F\/ '{print $2}'`
